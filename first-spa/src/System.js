dbb507c745261863c12ef0e62d8e83bd90931d8d
Winson_Mak <m.howai_stuff@yahoo.com.hk>
2022-07-30 09:29:18 +0800

    hello

--- first-spa/src/System.js
+++ first-spa/src/System.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import axios from 'axios'
 
 export default function System() {
 
@@ -11,23 +12,33 @@ function handleChange(event){
     console.log(event.target.value)
 }
 function handleChange_02(event){
-    setinternval(event.target.value)
-    console.log(event.target.value)
-}
-function handleChange_03(event){
     setnumber(event.target.value)
     console.log(event.target.value)
 }
+function handleChange_03(event){
+    setinternval(event.target.value)
+    console.log(event.target.value)
+}
 function handleSubmit(event){
-    console.log(device_id)
-    console.log(interval)
-    console.log(number)
+
+    let text = number + " " + interval
+    axios({ 
+        method: 'get' , 
+        url: ' https://coqaxw2nncra5xh6fmxhgsn4la0gnjbu.lambda-url.ap-east-1.on.aws/', 
+        responseType: 'json',
+        'Content-Type': 'application/json',
+        data: {
+            "function_name" : "from_now",
+            "device_id": "87",
+            "date_range": "2 HOUR"
+          }
+       }) 
+    .then((res) => { console.table(res.data) })
+    .catch((error) => { console.error(error) })
+    .finally(() => { })
 }
 
-// axios.get('https://coqaxw2nncra5xh6fmxhgsn4la0gnjbu.lambda-url.ap-east-1.on.aws/', { params: { device_id: device_id, date_range: number + interval} })
-// .then((res) => { console.table(res.data) })
-// .catch((error) => { console.error(error) })
-// .finally(() => { })
+
 
   return (
     
@@ -36,18 +47,18 @@ function handleSubmit(event){
     Device ID <br/>
     <select name="device_id" id="selectList" value={device_id} onChange={handleChange}>
           <option value="15">15</option>
-          <option value="5656">5656</option>
+          <option value="16">16</option>
     </select> 
     <br/><br/>
     Time from now <br/>
-    <select name="number" id="selectList" value={interval} onChange={handleChange_02}>
+    <select name="number" id="selectList" value={number} onChange={handleChange_02}>
           <option value="1">1</option>
           <option value="2">2</option>
     </select>
-    <select name="interval" id="selectList" value={number} onChange={handleChange_03}>
-          <option value="Minute">Minute</option>
-          <option value="Hour">Hour</option>
-          <option value="Day">Day</option>
+    <select name="interval" id="selectList" value={interval} onChange={handleChange_03}>
+          <option value="MINUTE">Minute</option>
+          <option value="HOUR">Hour</option>
+          <option value="DAY">Day</option>
     </select>
     <br/>
     <br/>
